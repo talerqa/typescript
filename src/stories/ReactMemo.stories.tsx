20c5645e8cc3ccb2c154bf6672abacbf226715a2
talerqa <hitri3003@gmail.com>
2023-05-22 21:01:07 +0300

    add: complete 17task useMemo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+export default {
+  title: 'React.Memo demo'
+}
+
+const NewMessagesCounter = (props: any) => {
+  console.log('NEWMESSCOUNTER')
+  return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+  console.log('USERS')
+  return <div>
+    {props.users.map((u, i) => <div key={i}>{u}</div>)}
+  </div>
+}
+
+const User = React.memo(UsersSecret)
+const MessagesCounter = React.memo(NewMessagesCounter)
+
+export const Example1 = () => {
+
+  const [counter, setCounter] = useState(0)
+  const [users, setUsers] = useState(['Lesha', 'MIsha', 'Dima'])
+
+  const addUser = () => {
+    const newUser = [...users, 'fsdfsdf']
+    setUsers(newUser)
+  }
+
+  return <>
+    <button onClick={() => setCounter(counter + 1)}>INC
+    </button>
+    <button onClick={addUser}>Add user</button>
+    <MessagesCounter count={counter}/>
+    <User users={users}/>
+  </>
+
+}
