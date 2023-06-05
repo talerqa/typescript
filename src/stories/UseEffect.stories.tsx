cfc92fa1750da6594babf1c59fe3246d16b8313a
talerqa <hitri3003@gmail.com>
2023-06-05 21:09:56 +0300

    add: clock useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -27,20 +27,46 @@ export const SetTimeoutExample = () => {
   const [fake, setFake] = useState(1)
 
   useEffect(()=>{
-    console.log('useEffect')
 
     setInterval(()=>{
-      console.log('Interval')
       setCounter((state)=> state + 1)
     },1000)
   },[] )
 
 
+  return (<div>
+    <button onClick={() => {
+      setFake(fake + 1)
+    }}> +
+    </button>
+    fake {fake}
+    <button onClick={() => {
+      setCounter(counter + 1)
+    }}> +
+    </button>
+    counter {counter}
+  </div>)
+}
+
+export const NewDate = () => {
+  console.log('Component')
+
+  const [hours, setHours] = useState(new Date().getHours())
+  const [minutes, setMinutes] = useState(new Date().getMinutes())
+  const [second, setSecond] = useState(new Date().getSeconds())
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setHours(new Date().getHours())
+      setMinutes(new Date().getMinutes())
+      setSecond(new Date().getSeconds())
+    }, 1000)
+
+  }, [])
+
 
   return (<div>
-    <button onClick={()=>{setFake(fake+1)}}> + </button>
-    fake {fake}
-    <button onClick={()=>{setCounter(counter+1)}}> + </button>
-    counter {counter}
+    {hours}:{minutes}:{second}
   </div>)
 }
